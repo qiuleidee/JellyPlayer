973dbe0e27e60d6007b629d9665fee16c96c00ee
JellyPlayer Dev <dev@jellyplayer.local>
2026-06-02 12:36:42 +0800

    🔧 fix: 引入 legacy 插件开启旧版 WebView 语法兼容

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'Android >= 5'],
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
